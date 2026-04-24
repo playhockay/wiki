62c3e34b761548a1899fa5a87d1bf27b18f38e7c
hockaycouncil <github@mail.hockay.com>
2026-04-24 16:07:44 -0400

    fix: keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -105,6 +105,15 @@ export default defineConfig({
 		"/editor/[...params]": "/keystatic/[...params]",
 	},
 	vite: {
+		define: {
+			"process.env.KEYSTATIC_GITHUB_CLIENT_ID": JSON.stringify(
+				process.env.KEYSTATIC_GITHUB_CLIENT_ID,
+			),
+			"process.env.KEYSTATIC_GITHUB_CLIENT_SECRET": JSON.stringify(
+				process.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+			),
+			"process.env.KEYSTATIC_SECRET": JSON.stringify(process.env.KEYSTATIC_SECRET),
+		},
 		plugins: [tailwindcss()],
 	},
 	markdown: {
